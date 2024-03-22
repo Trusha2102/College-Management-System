1a5984e722a197a39e358a5036ccb5b3e6f6d7c7
TrushaCH <trusha@creative-hustlers.com>
2024-03-22 14:46:11 +0530

    Prisma Files Removed

--- prisma/migrations/20240313093718_permission/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `operation` on table `Permission` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Permission" ALTER COLUMN "operation" SET NOT NULL;
