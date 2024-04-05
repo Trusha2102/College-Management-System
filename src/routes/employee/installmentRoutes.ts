9f88b223d0cd7e2626f14a208ef5dc607e993ee5
TrushaCH <hustler.trusha@gmail.com>
2024-04-05 18:38:13 +0530

    Added permissionProtect to Instalment

--- src/routes/employee/installmentRoutes.ts
+++ src/routes/employee/installmentRoutes.ts
@@ -5,13 +5,25 @@ import permissionProtect from '../../middlewares/permissionMiddleware';
 const router = express.Router();
 
 // router.post('/add', installmentController.createInstallment);
-router.put('/update/:id', installmentController.updateInstallment);
-router.delete('/delete/:id', installmentController.deleteInstallment);
+router.put(
+  '/update/:id',
+  permissionProtect,
+  installmentController.updateInstallment,
+);
+router.delete(
+  '/delete/:id',
+  permissionProtect,
+  installmentController.deleteInstallment,
+);
 router.get(
   '/list/:staffLoanId',
   permissionProtect,
   installmentController.getInstallmentsByStaffLoanId,
 );
-router.get('/view/:id', installmentController.getInstallmentById);
+router.get(
+  '/view/:id',
+  permissionProtect,
+  installmentController.getInstallmentById,
+);
 
 export default router;
